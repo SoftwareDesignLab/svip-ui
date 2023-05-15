86df508730621208649ad54b0665b55da5fcef42
“tina-dilorenzo” <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-05-15 16:45:57 -0400

    add in buttons, flex style, and manage sbom page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { ButtonComponent } from './shared/components/button/button.component';
+import { ManageSbomsPageComponent } from './features/manage-sboms/manage-sboms-page/manage-sboms-page.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, ButtonComponent, ManageSbomsPageComponent],
+  imports: [BrowserModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/features/manage-sboms/manage-sboms-page/manage-sboms-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-sboms-page',
+  templateUrl: './manage-sboms-page.component.html',
+  styleUrls: ['./manage-sboms-page.component.css']
+})
+export class ManageSbomsPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
