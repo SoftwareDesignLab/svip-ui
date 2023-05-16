2b2cfc0ae1dbad397eb45a75c2a6a8f7e214cedc
Max Stein <mhs8558@rit.edu>
2023-05-16 13:42:35 -0400

    Adding metrics page, side panel, and accordion files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,31 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ButtonComponent } from './shared/components/button/button.component';
-import { ManageSbomsPageComponent } from './features/manage-sboms/manage-sboms-page/manage-sboms-page.component';
+import { AccordionComponent } from './shared/components/accordion/accordion.component';
+import { SidePanelComponent } from './shared/components/side-panel/side-panel.component';
+import { ManageSbomsPageComponent } from './features/manage-sboms/manage-sboms-page.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { MetricsSidePanelComponent } from './features/metrics/metrics-side-panel/metrics-side-panel.component';
+import { MetricsBodyComponent } from './features/metrics/metrics-body/metrics-body.component';
+import { MetricsMainComponent } from './features/metrics/metrics-main/metrics-main.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, ManageSbomsPageComponent, SpinnerComponent],
-  imports: [BrowserModule, NgbModule, HttpClientModule],
+  declarations: [
+    AppComponent, 
+    ButtonComponent, 
+    ManageSbomsPageComponent, 
+    SpinnerComponent, 
+    MetricsSidePanelComponent, 
+    MetricsBodyComponent, 
+    MetricsMainComponent, 
+    AccordionComponent, 
+    SidePanelComponent,
+  ],
+  imports: [
+    BrowserModule,  
+    NgbModule, 
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
