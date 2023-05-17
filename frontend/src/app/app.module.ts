868b1fd5b5623bf2efeac82c0337bdb8f45933a4
“tina-dilorenzo” <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-05-17 11:54:10 -0400

    add in comparison components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { ManageSbomsPageComponent } from './features/manage-sboms/manage-sboms-page.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { ComparePageComponent } from './features/compare/compare-page/compare-page.component';
+import { CompareSidepanelComponent } from './features/compare/compare-sidepanel/compare-sidepanel.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, ManageSbomsPageComponent, SpinnerComponent],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    ManageSbomsPageComponent,
+    SpinnerComponent,
+    ComparePageComponent,
+    CompareSidepanelComponent,
+  ],
   imports: [BrowserModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/features/compare/compare-sidepanel/compare-sidepanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-sidepanel',
+  templateUrl: './compare-sidepanel.component.html',
+  styleUrls: ['./compare-sidepanel.component.css']
+})
+export class CompareSidepanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
