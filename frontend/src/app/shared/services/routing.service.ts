c64d365f35630ad0f014ebf11706c56405c60f3d
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-05-17 13:44:38 -0400

    Routing service as used multiple times

---
+++ frontend/src/app/shared/services/routing.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoutingService {
+
+  private options: { [name: string]: Object } = {
+    "manage": {
+      desc: "Create, Upload, and Edit SBOMs",
+      route: "/manage"
+    },
+    'analytics': {
+      desc: "Compare, View SBOMS, Check Vulnerabilities",
+      route: "/analytics"
+    }
+  }
+
+  constructor(private router: Router) {}
+
+  Redirect(route: string) {
+    this.router.navigate([route]);
+  }
+
+  GetOptionKeys(): string[] {
+    return Object.keys(this.options);
+  }
+
+  GetInfo(key: string) {
+    return this.options[key];
+  }
+}
