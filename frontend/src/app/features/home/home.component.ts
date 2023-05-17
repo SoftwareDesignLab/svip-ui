f8f58a934a5cef2432db5cb0fa0cf44e316d3996
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-05-17 14:02:07 -0400

    Implemented home

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from 'src/app/shared/services/routing.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  
+    constructor(public routing: RoutingService) {}
 }
