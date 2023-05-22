a2bf4d5cfdefdf805b0657eb68dc119a45146bd5
Tina DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-05-22 17:08:45 -0400

    documentation and code cleanup

--- frontend/src/app/features/compare/comparison/comparison.component.ts
+++ frontend/src/app/features/compare/comparison/comparison.component.ts
@@ -26,24 +26,40 @@ export class ComparisonComponent {
   filtered = false;
   markTarget = false;
 
+  /**
+   * Gets comparison results
+   */
   get compare() {
     return this.comparison?.comparisons ? this.comparison.comparisons : {};
   }
 
+  /**
+   * Gets comparison components
+   */
   get components() {
     return !this.comparison ? [] : Object.keys(this.comparison?.comparisons);
   }
 
+  /**
+   * Gets last sent files
+   */
   get lastSentFilePaths() {
     return this.dataHandler.lastSentFilePaths;
   }
 
+  /**
+   * Gets if comparison is still loading
+   */
   get isLoadingComparison(): boolean {
     return this.dataHandler.IsLoadingComparison();
   }
 
   constructor(private dataHandler: DataHandlerService) {}
 
+  /**
+   * Changes location based on where user wants to navigate to
+   * @param newLoc - desired location
+   */
   decreaseDepth(newLoc: string) {
     const index = this.path.indexOf(newLoc);
     this.path = this.path.slice(0, index + 1);
@@ -53,6 +69,10 @@ export class ComparisonComponent {
     }
   }
 
+  /**
+   * Gets comparison results
+   * @returns comparison
+   */
   getComparison() {
     if (this.comparison !== this.dataHandler.comparison) {
       this.path = ['Components'];
@@ -61,11 +81,21 @@ export class ComparisonComponent {
     return this.dataHandler.comparison;
   }
 
+  /**
+   * Gets specified SBOM from last send index
+   * @param index - index of SBOM
+   * @returns name of SBOM from index
+   */
   getAliasFromIndex(index: any) {
     let path = this.dataHandler.lastSentFilePaths[index];
     return this.dataHandler.getSBOMAlias(path);
   }
 
+  /**
+   * Gets vulnerability formats (cpes, swids, purls, etc)
+   * @param Identifier - type of format
+   * @returns all of currently selected version's format values
+   */
   getAttributes(attribute: Identifier) {
     if (this.version) {
       return Object.keys(this.version[attribute]);
@@ -73,6 +103,11 @@ export class ComparisonComponent {
     return [];
   }
 
+  /**
+   * Checks to see if value is in target sbom
+   * @param value - location and value to check in targetSBOM
+   * @returns true if value is in target; else false
+   */
   isInTarget(val: { component?: string; version?: string; value?: string }) {
     if (val.component) {
       if (val.version) {
@@ -86,6 +121,9 @@ export class ComparisonComponent {
     return false;
   }
 
+  /**
+   * Formats target SBOM
+   */
   getTargetSBOMValues() {
     if (!this.comparison?.targetSBOM) {
       return;
@@ -107,12 +145,19 @@ export class ComparisonComponent {
   //#region Filters
 
   /**
-  * Checks to see if 
-  */
+   * Checks to see if number is equal to the number of total comparison files
+   * @param num - number to check against comparison file count
+   * @returns var(--red) if comparison has conflicts, var(--green) if success
+   */
   passed(num: number) {
-    return num < this.lastSentFilePaths.length ? `--red` : `--green`;
+    return num < this.lastSentFilePaths.length ? `var(--red)` : `var(--green)`;
   }
 
+  /**
+   * Checks to see if component has a conflict
+   * @param component - name of component to check
+   * @returns true if conflict; else false
+   */
   componentHasConflict(component: string) {
     let isConflict = false;
     this.compare[component].forEach((version) => {
@@ -135,6 +180,10 @@ export class ComparisonComponent {
     return isConflict;
   }
 
+  /**
+   *  Checks if there is a component conflict with other files
+   *  @returns true if conflict; else false
+   */
   hasConflict(appearances: number) {
     if (!this.filtered) return true;
     return appearances < this.dataHandler.lastSentFilePaths.length;
