e9b418a4e940cf334c85c9d78375f374557e38fc
fm8191 <fm8191@rit.edu>
2023-06-28 11:18:20 -0400

    skeleton

--- frontend/src/app/features/viewer/viewer.component.ts
+++ frontend/src/app/features/viewer/viewer.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent {
+  files: File[] = [];
 
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    if (files) {
+      this.handleFiles(files);
+    }
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  onFileSelected(event: any): void {
+    const files = event.target.files;
+    this.handleFiles(files);
+  }
+
+  handleFiles(files: FileList | null): void {
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        this.files.push(files[i]);
+      }
+    }
+  }
 }
