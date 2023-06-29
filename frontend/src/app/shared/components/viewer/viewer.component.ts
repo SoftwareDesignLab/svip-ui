4184b8bdc685881ac6e79f8d062e6a5677ec1119
Justin Jantzi <jwj7297@rit.edu>
2023-06-29 16:32:44 -0400

    See selected content

--- frontend/src/app/shared/components/viewer/viewer.component.ts
+++ frontend/src/app/shared/components/viewer/viewer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from '../../services/routing.service';
 
 @Component({
   selector: 'app-viewer',
@@ -8,30 +9,8 @@ import { Component } from '@angular/core';
 export class ViewerComponent {
   files: File[] = [];
   pretty: boolean = false;
-  text: string = '';
 
-  onDrop(event: DragEvent): void {
-    event.preventDefault();
-    const files = event.dataTransfer?.files;
-    if (files) {
-      this.handleFiles(files);
-    }
-  }
+  constructor(public routing: RoutingService) {
 
-  onDragOver(event: DragEvent): void {
-    event.preventDefault();
-  }
-
-  onFileSelected(event: any): void {
-    const files = event.target.files;
-    this.handleFiles(files);
-  }
-
-  handleFiles(files: FileList | null): void {
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        this.files.push(files[i]);
-      }
-    }
   }
 }
