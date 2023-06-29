c3ef60ea99d5693f95d7f4823644510902f9277f
Justin Jantzi <jwj7297@rit.edu>
2023-06-29 16:23:22 -0400

    updated routing

--- frontend/src/app/shared/services/routing.service.ts
+++ frontend/src/app/shared/services/routing.service.ts
@@ -5,48 +5,21 @@ import { DataHandlerService } from './data-handler.service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class RoutingService {
+  private page: PAGES = PAGES.NONE;
+  public data: any;
 
-  private options: { [name: string]: any } = {
-    "home": {
-      route: "/home",
-      requireValidSBOM: 0
-    },
-    "manage": {
-      desc: "Create, Upload, and Edit SBOMs",
-      route: "/manage",
-      requireValidSBOM: 0
-    },
-    'metrics': {
-      desc: "Run tests on SBOMs",
-      route: "/metrics",
-      requireValidSBOM: 1
-    },
-    'compare': {
-      desc: "Compare SBOMs to a target",
-      route: "/compare",
-      requireValidSBOM: 2
-    },
-    'Vulnerabilities': {
-      desc: "Check for threats",
-      route: "/vulnerabilities",
-      requireValidSBOM: 1
-    }
+  GetPage(): PAGES {
+    return this.page;
   }
 
-  constructor(private router: Router, public DataHandler: DataHandlerService) {}
-
-  Redirect(key: string) {
-    if(this.DataHandler.GetValidSBOMs().length >= this.options[key].requireValidSBOM){
-      this.router.navigate([this.options[key].route]);
-    }
+  SetPage(page: PAGES) {
+    this.page = page;
   }
+}
 
-  GetOptionKeys(): string[] {
-    return Object.keys(this.options);
-  }
-
-  GetInfo(key: string) {
-    return this.options[key];
-  }
+export enum PAGES {
+  NONE = 0,
+  VIEW = 1
 }
