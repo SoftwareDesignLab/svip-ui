a687bc28ca8082027641f4acb8a700765d8854be
Tina DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-07-07 16:19:35 -0400

    use upload endpoint

--- frontend/src/app/shared/services/data-handler.service.ts
+++ frontend/src/app/shared/services/data-handler.service.ts
@@ -8,19 +8,12 @@ import { HttpParams } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataHandlerService implements OnInit {
-
   private ipc!: IpcRenderer;
   public lastSentFilePaths: string[] = [];
-
   private files: { [path: string]: SBOMInfo } = {};
-
-  public comparison!: Comparison | null;
-
-  private loadingComparison: boolean = false;
-  private loadingMetrics: boolean = false;
 
   public selectedQualityReport!: string;
 
@@ -36,75 +29,56 @@ export class DataHandlerService implements OnInit {
     }
   }
 
-  ngOnInit(): void {
-    this.getSavedSBOMNames().then((results) => {
-      if (Array.isArray(results)) {
-        results.forEach((fileName) => {
-          this.getSavedSBOM(fileName).then((data) => {
-            this.ValidateFile(fileName, data);
-          })
-        })
-      }
-    })
-  }
+  ngOnInit(): void {}
 
-  AddFiles(paths: string[]) {
+  async AddFiles(paths: string[]) {
     paths.forEach((path) => {
-
       this.files[path] = {
         status: FileStatus.LOADING,
-      }
-
-      this.ValidateFile(path);
-    })
+      };
+      console.log(path);
+      this.ipc.invoke('getFileData', path).then((contents) => {
+        if (contents) {
+          this.saveSBOM(path, contents).subscribe(
+            (result) => {
+              if (result) {
+                this.files[path] = {
+                  status: FileStatus.VALID,
+                  id: result as number,
+                };
+              }
+            },
+            (error) => {
+              this.files[path] = {
+                status: FileStatus.ERROR,
+              };
+            }
+          );
+        }
+      });
+    });
   }
 
-  RunAllMetrics() {
-    Object.keys(this.files).forEach((file) => {
-      this.ValidateFile(file, true);
-    })
+  /**
+   * Save an SBOM in the database for future use
+   * @param fileName File name
+   * @param contents contents of the sbom
+   */
+  saveSBOM(fileName: string, contents: string) {
+    return this.client.post('upload', {
+      contents: contents,
+      fileName: fileName,
+    });
   }
-
-  IsLoadingComparison() {
-    return this.loadingComparison;
-  }
-
-  IsLoadingMetrics() {
-    return this.loadingMetrics;
-  }
-
-  async ValidateFile(path: string, metrics: boolean = false, contents = '') {
-    if (metrics) {
-      this.loadingMetrics = true;
-    }
-
-    let data = contents === '' ? await this.ipc.invoke('getFileData', path) : contents;
-
-    this.client.post(metrics ? "qa" : "parse", { 'fileName': path, 'contents': data }).subscribe((result) => {
-      this.files[path].status = FileStatus.VALID;
-      this.files[path].contents = data;
-
-      if (metrics) {
-        this.loadingMetrics = false;
-        this.files[path].metrics = new QualityReport(result as test);
-      }
-
-      this.saveSBOM(path, data);
-    },
-      (error) => {
-        this.loadingMetrics = false;
-        this.files[path].status = FileStatus.ERROR;
-        this.files[path].extra = error.error;
-      })
-  }
-
 
   DeleteFile(path: string) {
     delete this.files[path];
   }
 
   GetSBOMsOfType(status: FileStatus) {
-    return Object.keys(this.files).filter((x) => this.files[x].status === status);
+    return Object.keys(this.files).filter(
+      (x) => this.files[x].status === status
+    );
   }
 
   GetAllFiles() {
@@ -119,45 +93,42 @@ export class DataHandlerService implements OnInit {
     return this.GetSBOMInfo(path).metrics;
   }
 
-
   getSBOMAlias(path: string) {
     const pathChar = path.indexOf('/') !== -1 ? '/' : '\\';
     return path.split(pathChar).pop();
   }
 
-  
-  async downloadSBOM(id: number){
+  async downloadSBOM(id: number) {
     let cpn = 0;
     let filename = '';
     let content = '';
     let tpn = 1;
-    while( cpn != tpn) {
-    // increase cpn count
-    ++cpn;
-    // downlod file
-    let download: any = await firstValueFrom(this._downloadSBOM(id, cpn));
-    // set file name
-    filename = download.fileName;
-    // concat file contents 
-    content += download.contents;
-    // set var tpn to response's tpn
-    tpn = download.tpn;
+    while (cpn != tpn) {
+      // increase cpn count
+      ++cpn;
+      // downlod file
+      let download: any = await firstValueFrom(this._downloadSBOM(id, cpn));
+      // set file name
+      filename = download.fileName;
+      // concat file contents
+      content += download.contents;
+      // set var tpn to response's tpn
+      tpn = download.tpn;
     }
-    
+
     // return fully downloaded file
-    const file = { cpn, filename, content, tpn }
+    const file = { cpn, filename, content, tpn };
     console.log(file);
   }
 
   private _downloadSBOM(id: number, cpn: number) {
     const params = new HttpParams({
       fromObject: {
-        'id': id,
-        'cpn': cpn
-      }
+        id: id,
+        cpn: cpn,
+      },
     });
-    return this.client.post('download', undefined, params)
-
+    return this.client.post('download', undefined, params);
 
     // const response = TEST_DATA.JSON;
     // const file = new File([response.content], response.fileName, {
@@ -176,48 +147,9 @@ export class DataHandlerService implements OnInit {
     //this.client.post("download", {}, params);
   }
 
-
-  async Compare(main: string, others: string[]): Promise<any> {
-    this.loadingComparison = true;
-    let paths = [main, ...others];
-    let files: File[] = [];
-
-    for (let i = 0; i < paths.length; i++) {
-      let path = paths[i];
-      let data = await this.ipc.invoke('getFileData', path);
-
-      files.push({
-        'fileName': path,
-        'contents': data
-      })
-    }
-
-    this.lastSentFilePaths = paths;
-
-    return this.client.post("compare", files, new HttpParams().set('targetIndex', 0));
-
-  }
-
-  generateSBOM(fileContents: string[], fileNames: string[], schema?: string, format?: string) {
-    return this.client.post("generate", { 'fileContents': fileContents, 'fileNames': fileNames, 'schemaName': schema, 'formatName': format });
-  }
-
-  merge(fileContents: string[], fileNames: string[], schema: string, format: string) {
-    return this.client.post("merge", { 'fileContents': fileContents, 'fileNames': fileNames, 'schema': schema, 'format': format });
-  }
-
-  /**
-   * Save an SBOM in the database for future use
-   * @param fileName File name
-   * @param contents contents of the sbom
-   */
-  saveSBOM(fileName: string, contents: string) {
-    return this.client.post("upload", { 'contents': contents, 'fileName': fileName });
-  }
-
   getSavedSBOMNames(): Promise<Object> {
     return new Promise<Object>((resolve) => {
-      this.client.get("viewFiles").subscribe((result) => {
+      this.client.get('viewFiles').subscribe((result) => {
         resolve(result);
       });
     });
@@ -225,9 +157,11 @@ export class DataHandlerService implements OnInit {
 
   getSavedSBOM(name: string) {
     return new Promise<any>((resolve) => {
-      this.client.get("view", new HttpParams().set("id", name)).subscribe((result) => {
-        resolve(result);
-      });
+      this.client
+        .get('view', new HttpParams().set('id', name))
+        .subscribe((result) => {
+          resolve(result);
+        });
     });
   }
 }
@@ -239,6 +173,7 @@ export interface File {
 
 export interface SBOMInfo {
   status: FileStatus;
+  id?: number;
   metrics?: QualityReport;
   extra?: string;
   contents?: string;
@@ -247,11 +182,11 @@ export interface SBOMInfo {
 export enum FileStatus {
   LOADING,
   ERROR,
-  VALID
+  VALID,
 }
 
 const TEST_DATA = {
-  "JSON": { "fileName": "sbom.json", "content": "{\"bomFormat\" : \"CycloneDX\"}" },
-  "XML": { "fileName": "sbom.xml", "content": "SPDXVersion : SPDX-2.2" },
-  "SPDX": { "fileName": "sbom.spdx", "content": "" },
-}
+  JSON: { fileName: 'sbom.json', content: '{"bomFormat" : "CycloneDX"}' },
+  XML: { fileName: 'sbom.xml', content: 'SPDXVersion : SPDX-2.2' },
+  SPDX: { fileName: 'sbom.spdx', content: '' },
+};
