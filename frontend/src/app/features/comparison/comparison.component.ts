9c0553365a7596ab990e541c3739ba256f6cc4b0
Justin Jantzi <jwj7297@rit.edu>
2023-07-14 16:01:14 -0400

    Finished compare component

--- frontend/src/app/features/comparison/comparison.component.ts
+++ frontend/src/app/features/comparison/comparison.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataHandlerService } from 'src/app/shared/services/data-handler.service';
 
 @Component({
   selector: 'app-comparison',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ComparisonComponent {
 
+  constructor(public dataHandler: DataHandlerService) { }
+
+  GetComparison() {
+    return this.dataHandler.comparison;
+  }
+
+  protected castToString(value: unknown): string {
+    return value as string;
+  }
+
+  protected formatValue(value: any): string {
+    const formattedValue = JSON.stringify(value, null, 2);
+    return formattedValue.replace(/[[\]{},"]/g, '') + '\n';
+  }
+
+  protected castAsAny(value: unknown): any {
+    return value as any;
+  }
 }
