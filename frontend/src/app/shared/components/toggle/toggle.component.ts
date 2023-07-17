90ab0cbeedd3d7cb916c07f15c2668af55baa6f1
maxstein16 <mhs8558@rit.edu>
2023-07-17 10:53:31 -0400

    adding generate drop to the viewer

--- frontend/src/app/shared/components/toggle/toggle.component.ts
+++ frontend/src/app/shared/components/toggle/toggle.component.ts
@@ -10,6 +10,20 @@ import { IpcRenderer } from 'electron';
 export class ToggleComponent {
   upload: boolean = false;
   private ipc!: IpcRenderer;
+  public selectedFileAlias: string | undefined = undefined;
+  public selectedFilePath: string | undefined = undefined;
+  public generateModal: boolean = false;
+  public convertOptions: {
+    schema: string,
+    format: string
+    overwrite: boolean
+  } = {
+    schema: '',
+    format: '',
+    overwrite: true,
+  };
+  public schemaOptions: string[] = ['TAGVALUE', "JSON"];
+  public formatOptions: string[] = ['CDX14', "SPDX23", "SVIP"];
 
   constructor(private dataHandler: DataHandlerService) {
     if (window.require) {
@@ -31,5 +45,25 @@ export class ToggleComponent {
 
       this.dataHandler.AddFiles(files);
     });
+  }
+
+  /**
+   * Get SBOM filename
+   */
+  getAlias(sbom: string) {
+    return this.dataHandler.getSBOMAlias(sbom);
+  }
+
+  onGenerateDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      const filePath = files[0].path;
+      this.selectedFilePath = filePath;
+      this.selectedFileAlias = this.getAlias(filePath);
+      this.generateModal = true;
+    }
   }
 }
