a08961dc3589eb2f3413d42583a469eb88b07be5
Max Stein <mhs8558@rit.edu>
2023-07-19 11:54:25 -0400

    adding toolbar to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MenuComponent } from './shared/components/menu/menu.component';
 import { AccordionComponent } from './shared/components/accordion/accordion.component';
 import { ToastComponent } from './shared/components/toast/toast.component';
 import { ComparisonComponent } from './features/comparison/comparison.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ComparisonComponent } from './features/comparison/comparison.component'
     MenuComponent,
     ModalComponent,
     ToastComponent,
-    ComparisonComponent
+    ComparisonComponent,
+    ToolbarComponent
   ],
   imports: [BrowserModule, NgbModule, HttpClientModule, NgbTooltipModule, FormsModule],
   providers: [],
