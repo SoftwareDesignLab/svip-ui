63ea89d9d893344b4fb1f2cae040449db24efcbe
Max Stein <mhs8558@rit.edu>
2023-07-19 11:54:16 -0400

    removing unused code in upload which is now in the toolbar component

--- frontend/src/app/features/upload/upload.component.ts
+++ frontend/src/app/features/upload/upload.component.ts
@@ -14,26 +14,9 @@ import { PAGES, RoutingService } from 'src/app/shared/services/routing.service';
 export class UploadComponent implements OnInit {
   private ipc!: IpcRenderer;
   private filterSearch: string = '';
-  public deleteModal: boolean = false;
   public show: boolean = false;
 
-  public convertModal: boolean = false;
   public downloadModal: boolean = false;
-
-  public convertOptions: {
-    schema: string;
-    format: string;
-    overwrite: boolean;
-  } = {
-    schema: '',
-    format: '',
-    overwrite: true,
-  };
-  public formatOptions: string[] = ['TAGVALUE', 'JSON'];
-  public schemaOptions: string[] = ['CDX14', 'SPDX23', 'SVIP'];
-
-  public compareModal: boolean = false;
-  protected compareTarget: string = '';
 
   protected sortingOptions: { [type: string]: boolean } = {
     NAME: true,
@@ -133,18 +116,6 @@ export class UploadComponent implements OnInit {
     this.dataHandler.SetSBOMFormat(event.name, event.value);
   }
 
-  /**
-   * Removes file from uploaded files
-   * @param file file to remove
-   */
-  RemoveFile(file: string) {
-    if (this.routing.GetPage() === PAGES.VIEW && this.routing.data === file) {
-      this.routing.SetPage(PAGES.NONE);
-      this.routing.data = undefined;
-    }
-
-    this.dataHandler.deleteFile(file);
-  }
 
   setAllSelected(event: any) {
     let value = event.target.checked;
@@ -161,85 +132,6 @@ export class UploadComponent implements OnInit {
 
     this.sortingOptions[sort] = !this.sortingOptions[sort];
     this.selectedSorting = sort;
-  }
-
-  GetSelected() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    let selected: string[] = [];
-
-    for (let i = 0; i < checkboxes.length; i++) {
-      const checkbox = checkboxes[i] as HTMLInputElement;
-      if (checkbox.checked && !checkbox.disabled && checkbox.value != 'null') {
-        selected.push(checkbox.value);
-      }
-    }
-
-    return selected;
-  }
-
-  DeleteSelected() {
-    this.GetSelected().forEach((file) => {
-      this.RemoveFile(file);
-    });
-
-    this.deleteModal = false;
-  }
-
-  CompareSelected() {
-    this.routing.SetPage(2);
-
-    let others = this.GetSelected().filter((x) => x !== this.compareTarget);
-    this.dataHandler.CompareSBOMs(this.compareTarget, others);
-
-    this.compareModal = false;
-  }
-
-  DownloadSelected() {
-    const selectedFiles = this.GetSelected();
-    const hasFiles = selectedFiles.length;
-    const hasErroredFiles = selectedFiles.filter((sbom) => this.GetSBOMInfo(sbom).status === FileStatus.ERROR).length;
-
-    if (!hasFiles || hasErroredFiles) {
-      this.downloadModal = true;
-      setTimeout(() => {
-        this.downloadModal = false;
-      }, 4000);
-      return;
-    }
-
-    for (let i = 0; i < selectedFiles.length; i++) {
-      const file = selectedFiles[i];
-      const name = this.GetSBOMInfo(file).fileName;
-      const sbom = this.dataHandler.downloadSBOM(file);
-      if (sbom) {
-        const url = URL.createObjectURL(sbom);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = name as string;
-
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        window.URL.revokeObjectURL(url);
-      }
-      break;
-    }
-  }
-
-  ConvertSelected() {
-    if (this.convertOptions.schema === '' || this.convertOptions.format === '')
-      return;
-
-    this.GetSelected().forEach((file) => {
-      this.dataHandler.ConvertSBOM(
-        file,
-        this.convertOptions.schema,
-        this.convertOptions.format,
-        this.convertOptions.overwrite
-      );
-    });
-
-    this.convertModal = false;
   }
 
   /**
@@ -303,14 +195,6 @@ export class UploadComponent implements OnInit {
     return this.filterSearch;
   }
 
-  ViewSBOM() {
-    let selected = this.GetSelected();
-
-    if (selected.length !== 1) return;
-
-    this.routing.SetPage(PAGES.VIEW);
-    this.routing.data = selected[0];
-  }
 }
 
 export enum SORT_OPTIONS {
