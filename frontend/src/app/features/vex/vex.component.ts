50cc6cad392a6a2365bdf210b9ca5fab033f6b1d
Justin Jantzi <jwj7297@rit.edu>
2023-07-26 15:08:05 -0400

    Removed mock, added null message

--- frontend/src/app/features/vex/vex.component.ts
+++ frontend/src/app/features/vex/vex.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { VexResponse } from 'src/app/shared/models/vex';
 
-import mockVex from './mock-vex';
-
 @Component({
   selector: 'app-vex',
   templateUrl: './vex.component.html',
   styleUrls: ['./vex.component.css']
 })
 export class VexComponent {
-  protected vex: VexResponse = mockVex;
+  protected vex: VexResponse | undefined;
 
   protected vexOptions = {
     databases: ['osv', 'nvd'],
