0d5162095332c2ca2456af2b8fe27e070dd95190
Justin Jantzi <jwj7297@rit.edu>
2023-07-26 14:58:34 -0400

    display vex object fully

--- frontend/src/app/features/vex/vex.component.ts
+++ frontend/src/app/features/vex/vex.component.ts
@@ -9,7 +9,7 @@ import mockVex from './mock-vex';
   styleUrls: ['./vex.component.css']
 })
 export class VexComponent {
-  private vex: VexResponse = mockVex;
+  protected vex: VexResponse = mockVex;
 
   protected vexOptions = {
     databases: ['osv', 'nvd'],
@@ -18,5 +18,37 @@ export class VexComponent {
     selectedDatabase: '',
     selectedFormat: '',
     apiKey: '',
+  }
+
+  protected vexObjectList = ['status', 'products', 'vulnerability'];
+
+  protected castAsAny(value: unknown): any {
+    return value as any;
+  }
+
+  protected formatValue(input: any): string {
+    if (Array.isArray(input)) {
+      const formattedString = input.map(obj =>
+        Object.entries(obj)
+          .map(([key, value]) => (value ? `${key}: ${value}` : ''))
+          .filter(str => str !== '')
+          .join('<br>')
+      ).join('<br>');
+      return formattedString;
+    } else if (typeof input === 'object' && input !== null) {
+      const formattedString = Object.entries(input)
+        .map(([key, value]) => (value ? `${key}: ${value}` : ''))
+        .filter(str => str !== '')
+        .join('<br>');
+      return formattedString;
+    } else {
+      return input.toString();
+    }
+  }
+
+  protected removeStatements(value: any) {
+    const copy = { ...value };
+    delete copy.vexstatements;
+    return [copy];
   }
 }
