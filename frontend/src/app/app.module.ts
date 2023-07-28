1b278f33afd488b960e2d9a694c941425abd063b
Tina DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2023-07-28 14:38:38 -0400

    Revert "move modals"
    This reverts commit 504e0bbab9c4b0f6ec5ddb90022ca408403a1b46.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,9 +17,9 @@ import { MenuComponent } from './shared/components/menu/menu.component';
 import { AccordionComponent } from './shared/components/accordion/accordion.component';
 import { ToastComponent } from './shared/components/toast/toast.component';
 import { ComparisonComponent } from './features/comparison/comparison.component';
-import { CompareModalComponent } from './features/upload/modals/compare-modal/compare-modal.component'
-import { ConvertModalComponent } from './features/upload/modals/convert-modal/convert-modal.component';
-import { DeleteModalComponent } from './features/upload/modals/delete-modal/delete-modal.component';
+import { CompareModalComponent } from './shared/components/toolbar/modals/compare-modal/compare-modal.component'
+import { ConvertModalComponent } from './shared/components/toolbar/modals/convert-modal/convert-modal.component';
+import { DeleteModalComponent } from './shared/components/toolbar/modals/delete-modal/delete-modal.component';
 import { ViewerComponent } from './shared/components/viewer/viewer.component';
 import { ToasterComponent } from './shared/components/toaster/toaster.component';
 import { VexComponent } from './features/vex/vex.component';
