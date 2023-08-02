385629132162d5815c8ec98ab1644a90689fff4f
Justin Jantzi <jwj7297@rit.edu>
2023-08-02 09:21:19 -0400

    display folder selection and create temp zip

--- frontend/app.js
+++ frontend/app.js
@@ -52,37 +52,17 @@ ipcMain.handle("selectFiles", async () => {
   return files.filePaths;
 });
 
-ipcMain.handle("getFolderDirectory", async () => {
-  return new Promise(async(resolve, reject) => {
-    let folder = await dialog.showOpenDialog(mainWindow, {
-      properties: ["openDirectory"],
-    });
-  
-    if(folder.filePaths.length === 1) {
-      let zipPath = folder.filePaths[0];
-      let tempPath = path.join(__dirname, "temp.zip");
+ipcMain.handle("getZipFromFolder", async () => {
+  let folder = await dialog.showOpenDialog(mainWindow, {
+    properties: ["openDirectory"],
+  });
 
-      zipPaths = [zipPath, tempPath];
-
-      return resolve(true);
-    }
-
-    return reject(false);
-  })
+  if(folder.filePaths.length === 1) {
+    let zipPath = folder.filePaths[0];
+    let tempPath = path.join(__dirname, "temp");
+    await zip(zipPath, tempPath);
+  }
 });
-
-ipcMain.handle("zipDirectory", async() => {
-
-  return new Promise(async(resolve, reject) => {
-    try {
-      await zip(zipPaths[0], zipPaths[1]);
-      const fileData = await fs.promises.readFile(zipPaths[1]);
-      return resolve(fileData);
-    } catch(error) {
-      return reject(error);
-    }
-  })
-})
 
 ipcMain.handle("getFileData", async (event, ...args) => {
   let data = fs.readFileSync(args[0], "utf8");
