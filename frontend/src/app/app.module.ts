a11b2a3e12de72caa22064e3a9115db54e3cc178
Justin Jantzi <jwj7297@rit.edu>
2023-08-03 04:58:38 -0400

    Generate Modal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,9 +29,7 @@ import { MetricsComponent } from './features/metrics/metrics/metrics.component';
 import { MergeModalComponent } from './shared/components/toolbar/modals/merge-modal/merge-modal.component';
 import { RepairModalComponent } from './shared/components/toolbar/modals/repair-modal/repair-modal.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-import { DiffFiltersComponent } from './features/comparison/diff-filters/diff-filters.component';
 import { GenerateModalComponent } from './shared/components/toolbar/modals/generate-modal/generate-modal.component';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -59,9 +57,7 @@ import { SpinnerComponent } from './shared/components/spinner/spinner.component'
     MergeModalComponent,
     RepairModalComponent,
     SpinnerComponent,
-    DiffFiltersComponent,
     GenerateModalComponent,
-    SpinnerComponent
   ],
   imports: [BrowserModule, NgbModule, HttpClientModule, NgbTooltipModule, FormsModule],
   providers: [],
