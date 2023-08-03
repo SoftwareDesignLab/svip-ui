8f0bf7a4229aa55534f173f5e46ff838a51b9c3a
Justin Jantzi <jwj7297@rit.edu>
2023-08-03 04:58:38 -0400

    Generate Modal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { CircleToggleComponent } from './shared/components/circle-toggle/circle-
 import { QaFiltersComponent } from './features/metrics/qa-filters/qa-filters.component';
 import { MetricsComponent } from './features/metrics/metrics/metrics.component';
 import { MergeModalComponent } from './shared/components/toolbar/modals/merge-modal/merge-modal.component';
+import { GenerateModalComponent } from './shared/components/toolbar/modals/generate-modal/generate-modal.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import { MergeModalComponent } from './shared/components/toolbar/modals/merge-mo
     CircleToggleComponent,
     QaFiltersComponent,
     MergeModalComponent,
+    GenerateModalComponent,
   ],
   imports: [BrowserModule, NgbModule, HttpClientModule, NgbTooltipModule, FormsModule],
   providers: [],
