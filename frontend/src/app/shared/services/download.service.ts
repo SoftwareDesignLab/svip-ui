32364840bdf39da78de0a410b608af02d33485b2
Max Stein <mhs8558@rit.edu>
2023-08-04 10:53:45 -0400

    adding download service

--- frontend/src/app/shared/services/download.service.ts
+++ frontend/src/app/shared/services/download.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import * as JSZip from 'jszip';
-import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
@@ -19,22 +17,5 @@ export class DownloadService {
     anchor.click();
 
     window.URL.revokeObjectURL(url);
-  }
-
-  DownloadAsZip(files: {[key: string]: string}) {
-    const zip = new JSZip();
-
-    let keys = Object.keys(files);
-
-    for(let i = 0; i < keys.length; i++) {
-      let key = keys[i];
-      let contents = files[key];
-
-      zip.file(key, contents);
-    }
-
-    zip.generateAsync({ type: 'blob' }).then((content) => {
-    saveAs(content, 'files.zip');
-  });
   }
 }
