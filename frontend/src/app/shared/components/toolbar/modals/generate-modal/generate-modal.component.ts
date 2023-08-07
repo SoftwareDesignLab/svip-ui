7da246a4d5434f1691f4f7c1524aafe206855dcf
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-08-07 10:51:44 -0400

    Remove backend for now

--- frontend/src/app/shared/components/toolbar/modals/generate-modal/generate-modal.component.ts
+++ frontend/src/app/shared/components/toolbar/modals/generate-modal/generate-modal.component.ts
@@ -76,11 +76,9 @@ export class GenerateModalComponent implements OnInit {
     if (this.options.schema === '' || this.options.format === '' || this.options.type === '' || this.options.name === '')
       return;
 
-    this.service.uploadProject(this.zippedFileData,
-      this.options.name,
-      this.options.schema,
-      this.options.format,
-      this.options.type);
+    // this.service.uploadProjectDirectory(this.options.name, this.options.schema, this.options.format, this.options.type).then((result) => {
+    //   console.log(result);
+    // })
 
     this.Close();
   }
