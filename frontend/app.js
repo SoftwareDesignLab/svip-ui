308050d99788f2f812bda0006a58bc10b8100f38
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-08-11 14:52:44 -0400

    directory popup first for project

--- frontend/app.js
+++ frontend/app.js
@@ -56,12 +56,10 @@ ipcMain.handle("getZipFromFolder", async () => {
     let folder = await dialog.showOpenDialog(mainWindow, {
       properties: ["openDirectory"],
     });
-
     if(folder.filePaths.length === 1) {
       let zipPath = folder.filePaths[0];
       let tempPath = path.join(__dirname, "temp.zip");
       await zip(zipPath, tempPath);
-
       const fileData = await fs.promises.readFile(filePath);
       return resolve(fileData);
     }
