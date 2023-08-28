9d961e25c19135e3ca397299a768406ad0936d07
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-08-28 15:23:05 -0400

    OSI Generation request succeed

--- frontend/src/app/shared/services/client.service.ts
+++ frontend/src/app/shared/services/client.service.ts
@@ -40,6 +40,14 @@ export class ClientService {
     return this.http.post(this.SERVER_URL + path, body, this.httpOptions);
   }
 
+  postFile(path: string, body: any, params: HttpParams = new HttpParams()) {
+    let headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': 'http://localhost:4200'
+    });
+
+    return this.http.post(this.SERVER_URL + path, body, { headers: headers, params: params });
+  }
+
   put(path: string, params: HttpParams = new HttpParams()) {
     this.httpOptions.params = params;
     return this.http.put(this.SERVER_URL + path, null, this.httpOptions);
